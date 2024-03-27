190af7e298be326b5e84b2f5017e81bc300994ac
evan <evanmhelle@gmail.com>
2024-03-27 23:30:25 +0100

    affichage des rounds en console (reste à demander si le joueur pioche ou non)

--- Game.java
+++ Game.java
@@ -65,21 +65,42 @@ public class Game {
         bank =0;
         int sum_deck_player1 =0;
         int sum_deck_player2 =0;
+        int round = 0;
         //condition pour que la partie continue
         while(sum_deck_player1 <22 && sum_deck_player2 <22) {
+            sum_deck_player1 =0;
+            sum_deck_player2 =0;
+            round +=1;
+            System.out.println("\nround n°"+round);
 
             //les deux joueurs piochent une carte
             player1.draw_card(game_deck);
             dealer.draw_card(game_deck);
 
-            //recuperation de la taille actuelle du deck des joueur (c'est la meme pour les deux)
+            //recuperation de la taille actuelle du deck des joueurs
             int size_deck_player = player1.get_player_deck().get_deck().size();
+            int size_deck_dealer = dealer.get_player_deck().get_deck().size();
+
+            int value1 = player1.get_player_deck().get_deck().get(size_deck_player-1).get_value();
+            int value2 = dealer.get_player_deck().get_deck().get(size_deck_dealer-1).get_value();
+            String number1 = player1.get_player_deck().get_deck().get(size_deck_player-1).number_association(value1);
+            String number2 = dealer.get_player_deck().get_deck().get(size_deck_dealer-1).number_association(value2);
+            System.out.println("Carte pioché: "+ number1 +player1.get_player_deck().get_deck().get(size_deck_player-1).get_color());
+            System.out.println("Carte pioché par le croupier: "+ number2 +dealer.get_player_deck().get_deck().get(size_deck_dealer-1).get_color());
+
+            System.out.println("Votre main:");
+            player1.get_player_deck().display_deck();
+
+            System.out.println("Main du croupier:");
+            dealer.get_player_deck().display_deck();
 
             //recuperation de la valeur du deck de chaque joueur
             for (int i = 0; i < size_deck_player; i++) {
                 sum_deck_player1 += player1.get_player_deck().get_deck().get(i).get_value();
                 sum_deck_player2 += dealer.get_player_deck().get_deck().get(i).get_value();
             }
+            System.out.println("La somme de votre main est: "+sum_deck_player1);
+            System.out.println("La somme de la main du croupier: "+sum_deck_player2);
             if (sum_deck_player1 == 21){
 
             }
